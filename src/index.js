8e4a8ce8e5a347e1b5d7741f26754ee2580c2c04
tampro0123 <100013126+tampro0123@users.noreply.github.com>
2023-01-25 06:56:19 +0700

    edit babelrc

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import GlobalStyles from './components/GlobalStyles';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GlobalStyles>
+      <App />
+    </GlobalStyles>
   </React.StrictMode>
 );
 
