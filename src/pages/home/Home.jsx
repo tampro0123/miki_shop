9ccb61a8b7c6fcb6d1e4a56c0185e8747ca73038
tampro0123 <100013126+tampro0123@users.noreply.github.com>
2023-01-28 08:53:05 +0700

    Add Layout Footer

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,15 @@
+import Footer from "~/components/Layout/DefaultLayout/Footer";
 import Header from "~/components/Layout/DefaultLayout/Header";
+import styles from "./Home.module.scss";
+import classNames from "classnames/bind";
 
+const cx = classNames.bind(styles);
 function Home() {
     return (
         <>
-            <div className="wrapper">
+            <div className={cx("wrapper")}>
                 <Header />
+                <Footer />
             </div>
         </>
     );
