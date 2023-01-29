6c92e6b77522b4708672b4596d3d3b1c545b2022
tampro0123 <100013126+tampro0123@users.noreply.github.com>
2023-01-29 11:30:53 +0700

    add Product page

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,15 @@
 import Register from '~/pages/auth/register';
 import Home from '~/pages/home';
 import Login from '~/pages/auth/login/Login';
+import Products from '~/pages/products/Products';
 
 
 const publicRoutes = [
   { path: '/home', component: Home },
   { path: '/register', component: Register },
-  { path: '/login', component: Login }
+  { path: '/login', component: Login },
+  { path: '/products', component: Products },
+
 ]
 
 const privateRoutes = []
